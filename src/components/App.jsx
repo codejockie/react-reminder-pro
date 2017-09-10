f83edd2fbe153dc06358d18dde8e8432a1f6767e
John Kennedy (codejockie) <devjckennedy@gmail.com>
2017-09-10 17:38:02 +0100

    map dispatch to props bind action creators

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { addReminder } from '../actions/index.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: ''
+    }
+  }
+
+  addReminder() {
+    const { addReminder } = this.props;
+    const { text } = this.state;
+    
+    addReminder(text);
+  }
+
   render() {
     return (
       <div className="App">
@@ -12,11 +30,13 @@ class App extends Component {
             <input
               className="form-control"
               placeholder="I have to..."
+              onChange={event => this.setState({ text: event.target.value })}
             />
           </div>
           <button
             className="btn btn-success"
             type="button"
+            onClick={() => this.addReminder()}
           >
             Add Reminder  
           </button>
@@ -26,4 +46,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(null, { addReminder })(App);
